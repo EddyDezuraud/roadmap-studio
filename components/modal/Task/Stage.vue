d5d36c0e298ad31f059388d03df39b226166d720
EddyDezuraud <eddy.dezuraud@gmail.com>
2024-06-29 10:15:02 +0200

    Update stages and task

---
+++ components/modal/Task/Stage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div :class="$style.wrapper">
+    <select>
+      <option v-for="option in stagesOptions" :value="option.value">{{option.text}}</option>
+    </select>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+import type { Task, TaskStage } from '~/types/roadmap'
+import { roadmapStore } from '~/store/roadmap'
+
+const store = roadmapStore();
+
+interface Props {
+  stage: TaskStage;
+}
+
+</script>
+
+<style module>
+.wrapper {
+ display: flex;
+}
+</style>
